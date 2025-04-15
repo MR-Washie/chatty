aa585fe546e012938d163e7b15a4eedaf40d35e8
MR-Washie <mdragib.mth2005@gmail.com>
2025-04-15 22:23:30 +0530

    Update message.route.js

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -7,6 +7,9 @@ const router = express.Router();
 router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages);
 
-router.post("/send/:id", protectRoute, sendMessage);
+router.post("/send/:id", protectRoute, (req, res, next) => {
+    console.log("ID Parameter:", req.params.id);
+    next();
+}, sendMessage);
 
 export default router;
