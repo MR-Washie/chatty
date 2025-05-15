c26ab0a9bc8834ad7d1a20170cb713fa1c0a6c0f
MR-Washie <mdragib.mth2005@gmail.com>
2025-05-15 17:54:21 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   // baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-    baseURL: import.meta.env.MODE == "https://chattyyy.onrender.com/api",
+    baseURL: import.meta.env.MODE === "https://chattyyy.onrender.com/api",
       // baseURL: "https://chattyyy.onrender.com",
 
 
