9f34d695ebd07886be21a73f1f7eba85e0cc5134
MR-Washie <mdragib.mth2005@gmail.com>
2025-05-15 17:49:51 +0530

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,9 +4,9 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 // const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
-const BASE_URL = import.meta.env.MODE === "https://chattyyy.onrender.com";
+// const BASE_URL = import.meta.env.MODE === "https://chattyyy.onrender.com";
 
-// const BASE_URL = "https://chattyyy.onrender.com";
+const BASE_URL = "https://chattyyy.onrender.com";
 
 
 export const useAuthStore = create((set, get) => ({
